7933beb437f86c268b045314a49cc594243e1e9a
Pawel Relinski <pawelrelinski@gmail.com>
2023-04-24 19:03:10 +0200

    feature: Add Inoice model
    In User Detail you can see they invoice

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from core.models import User
+from core.models import User, Product, Invoice
 
 admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Invoice)
--- core/models.py
+++ core/models.py
@@ -20,10 +20,22 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    street_address = models.CharField(max_length=100, default='', null=False)
-    city = models.CharField(max_length=50, default='', null=False)
-    country = models.CharField(max_length=50, default='', null=False)
-    postal_code = models.CharField(max_length=20, default='', null=False)
+    street_address = models.CharField(
+        max_length=100,
+        default='',
+        null=False)
+    city = models.CharField(
+        max_length=50,
+        default='',
+        null=False)
+    country = models.CharField(
+        max_length=50,
+        default='',
+        null=False)
+    postal_code = models.CharField(
+        max_length=20,
+        default='',
+        null=False)
     contact_information = models.CharField(max_length=50)
     account_status = models.CharField(
         max_length=20,
@@ -37,3 +49,44 @@ class User(models.Model):
 
     def get_absolute_url(self):
         return reverse('user-detail', args=[str(self.id)])
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=50)
+    net_price = models.DecimalField(max_digits=10, decimal_places=2)
+    tax_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    gross_price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def get_absolute_url(self):
+        return reverse('service-detail', args=[str(self.id)])
+
+
+class Invoice(models.Model):
+    seller = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='seller_invoices')
+    purchaser = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='purchaser_invoices')
+    products = models.ManyToManyField(Product)
+    invoice_number = models.CharField(max_length=50)
+    sale_date = models.DateField()
+    date_of_issue = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.invoice_number}"
+
+    def get_absolute_url(self):
+        return reverse('invoice-detail', args=[str(self.id)])
+
