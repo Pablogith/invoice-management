8dc000c6c4691822b89dca90d3168e1dd115b6be
Pawel Relinski <pawelrelinski@gmail.com>
2023-04-30 15:41:11 +0200

    feature: Handle CRUD operations on Invoice and Product

--- core/urls.py
+++ core/urls.py
@@ -3,18 +3,25 @@ from . import views
 
 app_name = 'core'
 urlpatterns = [
-    # ex: /
     path('', views.IndexView.as_view(), name='index'),
-    # ex: /dashboard/
     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
-    # ex: /users-list/
+    # User
     path('users-list/', views.UsersListView.as_view(), name='users_list'),
-    # ex: /user-add/
     path('user-add/', views.UserAdd.as_view(), name='user_add'),
-    # ex: /user/5
     path('user-detail/<int:pk>/', views.UserDetailView.as_view(), name='user_detail'),
-    # ex: /user-delete/2
     path('user-delete/<int:pk>/', views.UserDelete.as_view(), name='user_delete'),
-    # ex: /user-update/2
     path('user-update/<int:pk>/', views.UserUpdate.as_view(), name='user_update'),
+    # Invoice
+    path('invoice-pdf/<int:invoice_id>', views.generate_invoice, name='invoice_pdf'),
+    path('invoice-list/', views.InvoicesListView.as_view(), name='invoices_list'),
+    path('invoice-detail/<int:pk>/', views.InvoiceDetailView.as_view(), name='invoice_detail'),
+    path('invoice-add/', views.InvoiceAdd.as_view(), name='invoice_add'),
+    path('invoice-update/<int:pk>/', views.InvoiceUpdate.as_view(), name='invoice_update'),
+    path('invoice-delete/<int:pk>/', views.InvoiceDelete.as_view(), name='invoice_delete'),
+    # Product
+    path('product-list/', views.ProductsListView.as_view(), name='products_list'),
+    path('product-detail/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('product-add/', views.ProductAdd.as_view(), name='product_add'),
+    path('product-update/<int:pk>/', views.ProductUpdate.as_view(), name='product_update'),
+    path('product-delete/<int:pk>/', views.ProductDelete.as_view(), name='product_delete'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
 from django.views import generic
 from django.urls import reverse_lazy
-from .models import User
+from reportlab.lib import colors
+from .models import User, Invoice, Product
+import io
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.units import inch
 
 
 class IndexView(generic.TemplateView):
@@ -9,6 +15,104 @@ class IndexView(generic.TemplateView):
 
 class DashboardView(generic.TemplateView):
     template_name = 'core/dashboard.html'
+
+
+class InvoicesListView(generic.ListView):
+    model = Invoice
+    template_name = 'core/invoice/invoices_list.html'
+    context_object_name = 'invoices_list'
+
+    def get_queryset(self):
+        return Invoice.objects.all()
+
+
+class InvoiceDetailView(generic.DetailView):
+    model = Invoice
+    template_name = 'core/invoice/invoice_detail.html'
+    context_object_name = 'invoice'
+
+
+class InvoiceAdd(generic.CreateView):
+    model = Invoice
+    fields = [
+        'seller',
+        'purchaser',
+        'products',
+        'invoice_number',
+        'sale_date',
+        'date_of_issue',
+    ]
+    template_name = 'core/invoice/invoice_add.html'
+    success_url = reverse_lazy('core:invoices_list')
+
+
+class InvoiceUpdate(generic.UpdateView):
+    model = Invoice
+    fields = [
+        'seller',
+        'purchaser',
+        'products',
+        'invoice_number',
+        'sale_date',
+        'date_of_issue',
+    ]
+    template_name = 'core/invoice/invoice_update.html'
+    success_url = reverse_lazy('core:invoices_list')
+
+
+class InvoiceDelete(generic.DeleteView):
+    model = Invoice
+    template_name = 'core/invoice/invoice_confirm_delete.html'
+    success_url = reverse_lazy('core:invoices_list')
+
+
+class ProductsListView(generic.ListView):
+    model = Product
+    template_name = 'core/product/products_list.html'
+    context_object_name = 'products_list'
+
+    def get_queryset(self):
+        return Product.objects.all()
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'core/product/product_detail.html'
+    context_object_name = 'product'
+
+
+class ProductAdd(generic.CreateView):
+    model = Product
+    fields = [
+        'name',
+        'description',
+        'net_price',
+        'tax_rate',
+        'gross_price',
+        'amount'
+    ]
+    template_name = 'core/product/product_add.html'
+    success_url = reverse_lazy('core:products_list')
+
+
+class ProductUpdate(generic.UpdateView):
+    model = Product
+    fields = [
+        'name',
+        'description',
+        'net_price',
+        'tax_rate',
+        'gross_price',
+        'amount'
+    ]
+    template_name = 'core/product/product_update.html'
+    success_url = reverse_lazy('core:products_list')
+
+
+class ProductDelete(generic.DeleteView):
+    model = Product
+    template_name = 'core/product/product_confirm_delete.html'
+    success_url = reverse_lazy('core:products_list')
 
 
 class UsersListView(generic.ListView):
@@ -62,5 +166,57 @@ class UserUpdate(generic.UpdateView):
 
 class UserDelete(generic.DeleteView):
     model = User
+    template_name = 'core/user/user_confirm_delete.html'
     success_url = reverse_lazy('core:users_list')
+
+
+def generate_invoice(request, invoice_id):
+    invoice = Invoice.objects.get(id=invoice_id)
+    products = Product.objects.filter(invoice=invoice)
+    user = User.objects.get(id=invoice.purchaser.id)
+
+    buffer = io.BytesIO()
+    pdf = canvas.Canvas(buffer, pagesize=A4)
+    pdf.setFont("Helvetica-Bold", 14)
+    pdf.setFillColor(colors.black)
+
+    pdf.setFont('Helvetica-Bold', 16)
+    pdf.drawString(1 * inch, 10.5 * inch, 'Invoice')
+    pdf.setFont('Helvetica', 12)
+    pdf.drawString(1 * inch, 10.25 * inch, user.country)
+    pdf.drawString(1 * inch, 10 * inch, f'{user.city} {user.postal_code}, {user.street_address}')
+    pdf.drawString(1 * inch, 9.75 * inch, f'Phone: {user.contact_information}')
+    pdf.drawString(1 * inch, 9.5 * inch, f'Email: {user.email}')
+
+    pdf.setLineWidth(1)
+    pdf.line(1 * inch, 5.75 * inch, 8 * inch, 5.75 * inch)
+
+    pdf.setFont('Helvetica-Bold', 12)
+    pdf.drawString(1 * inch, 9 * inch, 'Item')
+    pdf.drawString(3 * inch, 9 * inch, 'Quantity')
+    pdf.drawString(4 * inch, 9 * inch, 'Price')
+    pdf.drawString(5 * inch, 9 * inch, 'Total')
+    pdf.line(1 * inch, 8.9 * inch, 6.5 * inch, 8.9 * inch)
+
+    y = 8.5 * inch
+    for product in products:
+        pdf.setFont('Helvetica', 12)
+        pdf.drawString(1 * inch, y, product.name)
+        pdf.drawString(3 * inch, y, str(product.amount))
+        pdf.drawString(4 * inch, y, '${:,.2f}'.format(product.gross_price))
+        pdf.drawString(5 * inch, y, '${:,.2f}'.format(product.amount * product.gross_price))
+        y -= 0.25 * inch
+
+    total = sum(product.amount * product.gross_price for product in products)
+    pdf.setFont('Helvetica-Bold', 12)
+    pdf.drawString(4 * inch, y - 0.5 * inch, 'Total:')
+    pdf.drawString(5 * inch, y - 0.5 * inch, '${:,.2f}'.format(total))
+
+    pdf.showPage()
+    pdf.save()
+
+    pdf_name = '%s.pdf' % invoice.invoice_number.replace(' ', '')
+
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename=pdf_name)
 
